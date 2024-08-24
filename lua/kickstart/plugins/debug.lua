9a91f8e1b39a8fc14bb4360557f3313c0b2015c2
Tyler Nieman <email@tyler.earth>
2024-08-24 07:04:17 -0700

    :sparkles: tyler's customizations

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -12,7 +12,23 @@ return {
   -- NOTE: And you can specify dependencies as well
   dependencies = {
     -- Creates a beautiful debugger UI
-    'rcarriga/nvim-dap-ui',
+    -- 'rcarriga/nvim-dap-ui',
+    {
+      'rcarriga/nvim-dap-ui',
+      dependencies = {
+        'mfussenegger/nvim-dap',
+        'nvim-neotest/nvim-nio',
+      },
+      keys = {
+        {
+          '<leader>du',
+          function()
+            require('dapui').toggle {}
+          end,
+          -- desc = desc 'Dap UI',
+        },
+      },
+    },
 
     -- Required dependency for nvim-dap-ui
     'nvim-neotest/nvim-nio',
@@ -22,7 +38,25 @@ return {
     'jay-babu/mason-nvim-dap.nvim',
 
     -- Add your own debuggers here
-    'leoluz/nvim-dap-go',
+    -- 'leoluz/nvim-dap-go',
+    {
+      'mxsdev/nvim-dap-vscode-js',
+      dependencies = {
+        'mfussenegger/nvim-dap',
+
+        -- TODO: might be duplicitous with mason installed version?
+        {
+          'microsoft/vscode-js-debug',
+          build = 'npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out',
+        },
+      },
+      opts = {
+        -- debugger_path = vim.fn.resolve(vim.fn.stdpath 'data' .. '/lazy/vscode-js-debug'),
+        -- debugger_path = '~/.local/share/nvim-kickstart' .. '/lazy/vscode-js-debug',
+        debugger_path = '/home/tyler/.local/share/nvim-kickstart/lazy/vscode-js-debug',
+        adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' },
+      },
+    },
   },
   keys = function(_, keys)
     local dap = require 'dap'
@@ -63,7 +97,8 @@ return {
       -- online, please don't ask me how to install them :)
       ensure_installed = {
         -- Update this to ensure that you have the debuggers for the langs you want
-        'delve',
+        -- 'delve',
+        'js',
       },
     }
 
@@ -89,17 +124,93 @@ return {
       },
     }
 
-    dap.listeners.after.event_initialized['dapui_config'] = dapui.open
-    dap.listeners.before.event_terminated['dapui_config'] = dapui.close
-    dap.listeners.before.event_exited['dapui_config'] = dapui.close
+    -- dap.listeners.after.event_initialized['dapui_config'] = dapui.open
+    -- dap.listeners.before.event_terminated['dapui_config'] = dapui.close
+    -- dap.listeners.before.event_exited['dapui_config'] = dapui.close
 
     -- Install golang specific config
-    require('dap-go').setup {
-      delve = {
-        -- On Windows delve must be run attached or it crashes.
-        -- See https://github.com/leoluz/nvim-dap-go/blob/main/README.md#configuring
-        detached = vim.fn.has 'win32' == 0,
-      },
-    }
+    -- require('dap-go').setup {
+    --   delve = {
+    --     -- On Windows delve must be run attached or it crashes.
+    --     -- See https://github.com/leoluz/nvim-dap-go/blob/main/README.md#configuring
+    --     detached = vim.fn.has 'win32' == 0,
+    --   },
+    -- }
+
+    -- Typescript and React
+    for _, language in ipairs {
+      'typescript',
+      'typescriptreact',
+    } do
+      dap.configurations[language] = {
+        -- Next.js adapters
+        -- (These represent `launch.json` configs)
+        {
+          name = 'Next.js: debug server-side',
+          type = 'pwa-node',
+          request = 'launch',
+          cwd = '${workspaceFolder}',
+          runtimeExecutable = 'npm',
+          runtimeArgs = {
+            'run-script',
+            -- 'dev',
+            'dev:turbo',
+          },
+          sourceMaps = true,
+        },
+        {
+          name = 'Next.js: debug client-side',
+          type = 'pwa-chrome',
+          request = 'launch',
+          url = 'http://localhost:3000',
+          sourceMaps = true,
+        },
+
+        {
+          name = 'Launch file',
+          type = 'pwa-node',
+          request = 'launch',
+          program = '${file}',
+          cwd = '${workspaceFolder}',
+          sourceMaps = true,
+          preLaunchTask = 'tsc',
+        },
+        {
+          name = 'Attach to node process',
+          type = 'pwa-node',
+          request = 'attach',
+          processId = require('dap.utils').pick_process,
+          cwd = '${workspaceFolder}',
+          rootPath = '${workspaceFolder}',
+        },
+        {
+          name = 'Debug Jest Tests',
+          type = 'pwa-node',
+          request = 'launch',
+          runtimeExecutable = 'node',
+          runtimeArgs = {
+            './node_modules/jest/bin/jest.js',
+            '--runInBand',
+          },
+          rootPath = '${workspaceFolder}',
+          cwd = '${workspaceFolder}',
+          console = 'integratedTerminal',
+          internalConsoleOptions = 'neverOpen',
+        },
+        {
+          name = 'TS Node Launch',
+          type = 'pwa-node',
+          request = 'launch',
+          sourceMaps = true,
+          cwd = '${workspaceFolder}',
+          runtimeExecutable = 'node',
+          runtimeArgs = { '--nolazy', '-r', 'ts-node/register' },
+
+          args = { '--inspect', '${file}' },
+
+          skipFiles = { '<node_internals>/**', 'node_modules/**' },
+        },
+      }
+    end
   end,
 }
--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -6,7 +6,9 @@ return {
     config = function()
       local lint = require 'lint'
       lint.linters_by_ft = {
-        markdown = { 'markdownlint' },
+        -- markdown = { 'markdownlint' }, -- TODO: how the fuck do i get this working?
+        typescript = { 'eslint_d' },
+        typescriptreact = { 'eslint_d' },
       }
 
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
@@ -52,4 +54,42 @@ return {
       })
     end,
   },
+
+  {
+    'nvimtools/none-ls.nvim',
+    name = 'none-ls',
+    lazy = false,
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    event = { 'BufReadPre', 'BufNewFile' },
+    opts = function()
+      local null_ls = require 'null-ls'
+      local formatting = null_ls.builtins.formatting
+      local code_actions = null_ls.builtins.code_actions
+      -- local diagnostics = null_ls.builtins.diagnostics
+      -- local completion = null_ls.builtins.completion
+
+      -- TODO: Try this suggestion for eslint_d + buf formatting
+      -- https://github.com/jose-elias-alvarez/typescript.nvim/issues/57#issuecomment-1387148837
+
+      return {
+        root_dir = require('null-ls.utils').root_pattern('.null-ls-root', 'Makefile', '.git'),
+
+        sources = {
+          code_actions.gitsigns, -- provide code actions from Gitsigns
+          formatting.prettierd,
+          -- require 'none-ls.code_actions.eslint_d',
+        },
+      }
+    end,
+  },
+
+  -- for eslint_d (not using because eslint_d doesn't work with mason, apparently)
+  -- {
+  --   'nvimtools/none-ls.nvim',
+  --   dependencies = {
+  --     'nvimtools/none-ls-extras.nvim',
+  --   },
+  -- },
 }
